be2f3c271cf967ac0d73494af8e13b3e151a04a8
XMPPwocky <nttheis@gmail.com>
2015-11-05 11:59:47 -0800

    boilerplate, some validation code

--- src/backend.rs
+++ src/backend.rs
@@ -37,7 +37,18 @@ impl Backend {
     pub fn auth_request(&mut self, sessionid: &str) -> BackendResult<Option<SteamId>> { 
         let result: Option<u64> = try!(self.redis.get(format!("session_steamid:{}", sessionid)));
 
+        let steamid = result.map(SteamId::from_u64);
+        
+        debug_assert_eq!(steamid.and_then(SteamId::get_universe), Some(::steamid::Universe::Public));
+        debug_assert_eq!(steamid.and_then(SteamId::get_type), Some(::steamid::AccountType::Individual));
+
         Ok(result.map(SteamId::from_u64))
+    }
+
+    pub fn start_playing(&mut self, steamid: SteamId) -> BackendResult<()> {
+        // search servers, maybe put in favorite server and return
+        // maybe put in fallback server and queue for favorites
+        unimplemented!()
     }
 
     pub fn get_queue_status(&mut self, steamid: SteamId) -> BackendResult<QueueStatus> {
